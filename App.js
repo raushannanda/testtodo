a3c116b68ef5e0ef245e38fc0b2c92700e76854e
Raushan <raushan.kr806@gmail.com>
2020-11-20 15:58:30 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,60 @@
+import React,{useState} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  ScrollView,
+  TouchableOpacity
+} from 'react-native'
+const App = () => {
+  return(
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <View>
+        <TextInput 
+          autoCorrect={true} 
+          spellCheck={false} 
+          style={styles.input}
+          placeholder="Type Todo"
+          ></TextInput>
+        </View>
+        <View>
+        <TouchableOpacity onPress={()=>{console.log("Hello")}}>
+          <Text style={styles.addbtn}>Add</Text>
+        </TouchableOpacity>
+        </View>
+      </View>
+      <ScrollView>
+        <Text>Hii</Text>
+      </ScrollView>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    flexDirection:'column',
+    justifyContent:'center'
+
+  },
+  box:{
+     marginVertical:5,
+     flexWrap:'wrap',
+     flexDirection:'row',
+     alignSelf:'center',
+  },
+  addbtn:{
+    backgroundColor:'blue',
+    color:'white',
+    padding:15
+    
+  },
+  input:{
+    borderWidth:1,
+    width:200,
+    marginHorizontal:5
+  }
+})
+export default App;
